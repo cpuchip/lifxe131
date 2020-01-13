a3c5f97b7e4223313d7a98475b2086a99c084d7d
Craig O'Shannessy <craig@perfectlogic.com.au>
2020-01-13 11:46:56 +1100

    Fixed DMX channel handling (was out by 1 device plus 1 channel (i.e. was reading red as blue) Fixed brightness handling (was scaling hue, i.e. changing the color, instead of just scaling the brightness), now much more accurate and rich colors Simplfied the config section that users need to modify, changed to handle  arbitary number of bulbs instead of just 2 Simplified and refactored much of the code Fixed the shutdown handling to be more robust and simpler

--- e131_receive.py
+++ e131_receive.py
@@ -1,41 +1,44 @@
 import time
 import colorsys
-import threading
+#import threading
 import signal
 import sys
 
 import sacn
 from lifxlan import LifxLAN
 
-lightIdx = (0,3,3) # start stop step for incramenting through all your channels in DMX data
 
-max_bit = 65535
+########## START CONFIG SETTINGS ###########
+
+UNIVERSE = 2 # Which dmx universe it should be listening on
+
+first_channel = 0       # First DMX channel - 1 (to account for array position)
+
+numb_lights = 3     # How many lights you have on your network?  All must be in the DMX data
+
+max_bright = 0.7 # Brightness, from 0 - 1
+
+########### END CONFIG SETTINGS ###########
+
+channels_per_light = 3  # Red, Green, Blue
+scale_up = 65535 # This value will change what's concidered max brightness. minimum is 1 maximum is 65535
 max_rgb = 255
-
-numb_lights = 2 # How many lights you have on your network so the automated light discovery knows when it's found them all
-UNIVERSE = 1 # Which dmx universe it should be listening on
-max_bright = 65535 # This value will change what's concidered max brightness. minimum is 1 maximum is 65535
-
 # test power control
 print("Discovering lights...")
 lifx = LifxLAN(numb_lights)
+lights = lifx.get_lights()
 original_powers = lifx.get_power_all_lights()
 original_colors = lifx.get_color_all_lights()
-lights = lifx.get_lights()
 
-[l.get_label() for l in lights] # populate all the labels on the lights so we can sort them
+def dmx_channels(idx):
+	start = first_channel + idx * channels_per_light
+	end = start + channels_per_light
+	return start, end
 
-
-#print(lights[0].get_label())
-#print(lights[0])
-#print(lights[1].get_label())
-#print(lights[1])
-
-def label(l):
-	return l.label
-lights.sort(key=label)
-print(lights[0].label)
-print(lights[1].label)
+lights.sort(key=lambda l: l.get_label())
+for i, l in enumerate(lights):
+	startChannel, endChannel = dmx_channels(i)
+	print(f"{l.label} is assigned to DMX channels {startChannel + 1} to {endChannel}")
 
 print("Running e1.31 Lifx reciever")
 # provide an IP-Address to bind to if you are using Windows and want to use multicast
@@ -45,49 +48,33 @@ receiver.start()  # start the receiving thread
 # define a callback function
 @receiver.listen_on('universe', universe=UNIVERSE)  # listens on universe 1
 def callback(packet):  # packet type: sacn.DataPacket
-	#print("Recieved DMX data: ==========================")
-	#print(len(packet.dmxData))  # print the received DMX data
-	#print(packet.dmxData[0:12])
-	for idx in range(0,len(lights)):
+	print("Recieved DMX data: ==> ",packet.dmxData[(first_channel):(first_channel + numb_lights*channels_per_light)])
+	for idx, light in enumerate(lights):
 		# color is [Hue, Saturation, Brightness, Kelvin], duration in ms
-		start = lightIdx[0] + (idx + 1) * lightIdx[2]
-		end = lightIdx[1] + (idx + 1) * lightIdx[2]
-		#print(" ===== ====== =====")
-		#print(start)
-		#print(end)
-		r, g, b = packet.dmxData[start:end]
-		hsv = colorsys.rgb_to_hsv(r / max_rgb, g / max_rgb, b / max_rgb)
-		color = (hsv[0]*max_bit, hsv[1]*max_bit, hsv[2]*max_bright, 3500)
-		#print(" ====== light ====== ")
-		#print(idx)
-		#print(hsv)
-		#print(color)
-		lights[idx].set_color(color, 0, True)
+		start, end = dmx_channels(idx)
+		rgb = packet.dmxData[start:end]
+		hsb = [int(x * scale_up) for x in colorsys.rgb_to_hsv(*[c / max_rgb for c in rgb])]
+		hsb[2] = hsb[2] * max_bright # Adjust the brightness
+		color = tuple(hsb) + (3500,) # Add kelvin
+		print(f"{light.label:20} set to {color} from RGB{rgb}")
+		light.set_color(color, 0, True)
 
 # optional: if you want to use multicast use this function with the universe as parameter
-receiver.join_multicast(1)
+receiver.join_multicast(UNIVERSE)
 
-sigINT = False
+def cleanup():
+	receiver.stop()
+	print("Restoring original colors and power to all lights...")
+	for light in lights:
+		light.set_color(original_colors[light])
+		light.set_power(original_powers[light])
+
 def signal_handler(sig, frame):
-		print("You pressed Ctrl+C!")
-		receiver.stop()
-		sigINT = True
-		sys.exit()
+	print("You pressed Ctrl+C!")
+	cleanup()
+	sys.exit()
+
 signal.signal(signal.SIGINT, signal_handler)
 print("Press Ctrl+C to quit")
 
-while not sigINT:
-	if sigINT:
-		break
 
-receiver.stop()
-
-print("Restoring original color to all lights...")
-for lightIdx in original_colors:
-	color = original_colors[lightIdx]
-	lightIdx.set_color(color)
-
-print("Restoring original power to all lights...")
-for lightIdx in original_powers:
-	power = original_powers[lightIdx]
-	lightIdx.set_power(power)
